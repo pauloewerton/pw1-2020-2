faa34eb40046a68009d431c526ee4682dcd2b4f5
Paulo Ewerton <pauloewerton@icloud.com>
2021-04-05 18:54:25 +0000

    Aula 10

---
+++ aula10/blog/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
